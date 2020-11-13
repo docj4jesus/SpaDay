beb698350c4680a6515b45a057dd6c32756b4f75
Janell Renee Johnson <docj4jesus@gmail.com>
2020-11-12 21:03:58 -0600

    Studio Work Without Error Working

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+
+            if (newUser.Password == verify)
+            {
+                ViewBag.Username = newUser.Username;
+                return View("Index");
+            }
+            else 
+            {
+                ViewBag.error = "Passwords do not match. Please enter them again.";
+                ViewBag.Username = newUser.Username;
+                ViewBag.Email = newUser.Email;
+
+                return View("Add");
+            }
+        }
+
+    }
+}
